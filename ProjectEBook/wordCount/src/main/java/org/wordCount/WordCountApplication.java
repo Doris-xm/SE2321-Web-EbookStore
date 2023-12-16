9ae3ac5a8e210d2d2af2f2ace32d850fdec69447
Du Xinmin <114809783+Doris-xm@users.noreply.github.com>
2023-12-17 03:22:06 +0800

    微服务wordcount

---
+++ ProjectEBook/wordCount/src/main/java/org/wordCount/WordCountApplication.java
@@ -0,0 +1,14 @@
+package org.wordCount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WordCountApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordCountApplication.class, args);
+	}
+
+}
